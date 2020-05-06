57be503935f4fbd639c2d93130194d3a43df3bff
Francisco Martínez Lasaca <frml@itu.dk>
2020-05-06 09:30:27 +0200

    Simplify fuzz target

--- src/cpp_fuzztarget.cpp
+++ src/cpp_fuzztarget.cpp
@@ -12,71 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <chrono>
-#include <memory>
+#include <iostream>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
-using namespace std::chrono_literals;
-
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
-
-class MinimalPublisher : public rclcpp::Node
+std::string get_all_input()
 {
-public:
-  MinimalPublisher(std::string input)
-  : Node("cpp_fuzztarget")
-  {
-    round_ = 0;
-    input_ = input;
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    timer_ = this->create_wall_timer(200ms, std::bind(&MinimalPublisher::timer_callback, this));
-  }
-
-private:
-  void timer_callback()
-  {
-    if (round_ == 0) {
-
-    } else if (round_ == 1) {
-      // Send message
-      auto message = std_msgs::msg::String();
-      message.data = input_;
-      RCLCPP_INFO(this->get_logger(), "Fuzz target publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
-    } else {
-      exit(0);
-    }
-
-    round_++;
-  }
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-  std::string input_;
-  int round_;
-};
-
-std::string get_all_input() {
   std::string res = "";
   std::string line;
   bool first = true;
-  while (std::getline(std::cin, line))
-  {
+  while (std::getline(std::cin, line)) {
     if (first) first = false;
     else res.append("\n");
-
     res.append(line);
   }
   return res;
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>(get_all_input()));
-  rclcpp::shutdown();
+  auto node = rclcpp::Node::make_shared("talker");
+  auto chatter_pub = node->create_publisher<std_msgs::msg::String>("topic", 10);
+  rclcpp::Rate loop_rate(62500);
+
+  while (rclcpp::ok()) {
+    int connections = chatter_pub->get_subscription_count();
+    if (connections > 0) {
+      auto msg = std_msgs::msg::String();
+      msg.data = get_all_input();
+      chatter_pub->publish(msg);
+      // std::cout << "Publishing" << std::endl;
+      break;
+    } 
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+  }
+  // std::cout << "Dying" << std::endl;
   return 0;
 }
